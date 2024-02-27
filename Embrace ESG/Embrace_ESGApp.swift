c0c431ca1c796c005ecefa23e72dfa0fcddb7de0
ericaokamura <erica.okamura@yahoo.com.br>
2024-02-27 20:35:48 -0300

    Initial Commit

---
+++ Embrace ESG/Embrace_ESGApp.swift
@@ -0,0 +1,20 @@
+//
+//  Embrace_ESGApp.swift
+//  Embrace ESG
+//
+//  Created by Erica Okamura on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Embrace_ESGApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
