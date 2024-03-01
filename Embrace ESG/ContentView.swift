cb42f5a762362d1105fdcbb2b9fad2bf5fa0625f
ericaokamura <erica.okamura@yahoo.com.br>
2024-03-01 12:50:02 -0300

    primeiro commit

--- Embrace ESG/ContentView.swift
+++ Embrace ESG/ContentView.swift
@@ -11,80 +11,89 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var titulo: String = ""
+    @State private var descricao: String = ""
+    @State private var selection = 0
 
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        
+        ScrollView {
+            
+            VStack(spacing: 0) {
+                
+                Text("Embrace ESG")
+                    .foregroundColor(Color.indigo)
+                    .fontWeight(.bold)
+                    .font(.system(size: 40))
+                    .frame(width: 400, height: 300)
+                    .padding(40)
+
+                
+                Text("Título")
+                    .foregroundColor(Color.indigo)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .frame(width: 400, height: 300)
+                    .padding(-150)
+                
+                TextField("Dê um título para a sua atividade", text: $titulo)
+                    .frame(width: 350, height: 300)
+                    .font(.system(size: 16))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(-100)
+                
+                
+                Text("Descrição")
+                    .foregroundColor(Color.indigo)
+                    .font(.system(size: 18))
+                    .fontWeight(.bold)
+                    .frame(width: 400, height: 300)
+                    .padding(-100)
+                
+                
+                TextField("Descreva em poucas palavras a sua atividade", text: $descricao)
+                    .frame(width: 350, height: 300)
+                    .font(.system(size: 16))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(-150)
+                
+                Picker(selection: $selection,
+                       label: Text("Categorias")) {
+                    Text("Escolha uma categoria").tag(0)
+                        .foregroundColor(Color.indigo)
+                    Text("VOLUNTARIADO").tag(1)
+                        .foregroundColor(Color.indigo)
+                    Text("COLETA SELETIVA").tag(2)
+                        .foregroundColor(Color.indigo)
+                    Text("CULTIVO HORTA DOMÉSTICA").tag(3)
+                        .foregroundColor(Color.indigo)
+                    Text("PRÁTICA DE ATIVIDADES FÍSICAS").tag(4)
+                        .foregroundColor(Color.indigo)
+                    Text("HÁBITOS ALIMENTARES SAUDÁVEIS").tag(5)
+                        .foregroundColor(Color.indigo)
                 }
-                .onDelete(perform: deleteItems)
+                .accentColor(.indigo)
+                .padding(60)
+                
+                Button("Salvar") {
+                    
+                }
+                .foregroundColor(Color.indigo)
+                .fontWeight(.bold)
+                .padding(50)
+                
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            
+        }
+        .frame(width: 1000, height: 1000)
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+    
 }
